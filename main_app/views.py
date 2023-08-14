4e69a569e1a94727a0e9be203c19246b13166d7a
menth0s <marslite@pm.me>
2023-08-14 11:11:02 -0400

    Added cat list

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+#Baby step - Usually a Model is used 
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Carlita', 'breed': 'tortie', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 #Always use request in your views functions
 def home(request):
@@ -8,3 +14,8 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+        'cats' : cats
+    })
